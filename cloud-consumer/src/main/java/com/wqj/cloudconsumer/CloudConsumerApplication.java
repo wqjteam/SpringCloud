6d7a23310cc3a4499b6fb117fc72427c959deb54
githubwqj2 <2812930196@qq.com>
2018-10-30 18:04:23 +0800

    tijiaodaima

---
+++ cloud-consumer/src/main/java/com/wqj/cloudconsumer/CloudConsumerApplication.java
@@ -0,0 +1,12 @@
+package com.wqj.cloudconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudConsumerApplication.class, args);
+	}
+}
