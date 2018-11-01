cb6f144dabab8c3c7b87aff65084e48592e90c04
githubwqj2 <2812930196@qq.com>
2018-11-01 17:42:10 +0800

    tijiaodaima

--- cloud-provider/src/main/java/com/wqj/cloudserver/CloudServerApplication.java
+++ cloud-provider/src/main/java/com/wqj/cloudserver/CloudServerApplication.java
@@ -2,10 +2,10 @@ package com.wqj.cloudserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaServer
 public class CloudServerApplication {
 
 	public static void main(String[] args) {
--- eureka-server/src/main/java/com/wqj/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/wqj/eurekaserver/EurekaServerApplication.java
@@ -2,13 +2,14 @@ package com.wqj.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableEurekaServer
+
+@Enable
 @SpringBootApplication
 public class EurekaServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EurekaServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
 }
