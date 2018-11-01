c4e19bcb0ed18f50ee52f1f8f26e33d45ef66b6e
githubwqj2 <2812930196@qq.com>
2018-11-01 17:45:20 +0800

    tijiaodaima

--- eureka-server/src/main/java/com/wqj/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/wqj/eurekaserver/EurekaServerApplication.java
@@ -2,10 +2,10 @@ package com.wqj.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 
-@Enable
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerApplication {
 
