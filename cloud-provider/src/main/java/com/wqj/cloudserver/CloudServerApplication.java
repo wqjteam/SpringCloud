f922c208b121c21531442bac6260cf299c8f4640
githubwqj2 <2812930196@qq.com>
2018-11-06 14:12:19 +0800

    tijiaodaima

--- cloud-provider/src/main/java/com/wqj/cloudserver/CloudServerApplication.java
+++ cloud-provider/src/main/java/com/wqj/cloudserver/CloudServerApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CloudServerApplication {
+public class CloudProviderApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CloudServerApplication.class, args);
+		SpringApplication.run(CloudProviderApplication.class, args);
 	}
 }
