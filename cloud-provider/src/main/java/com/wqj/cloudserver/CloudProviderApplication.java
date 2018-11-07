96a06495c592ecd312f563bb310378ada6512785
githubwqj2 <2812930196@qq.com>
2018-11-07 15:05:51 +0800

    tijiaodaima

--- cloud-provider/src/main/java/com/wqj/cloudserver/CloudProviderApplication.java
+++ cloud-provider/src/main/java/com/wqj/cloudserver/CloudProviderApplication.java
@@ -2,8 +2,10 @@ package com.wqj.cloudserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CloudProviderApplication {
 
 	public static void main(String[] args) {
--- cloud-provider/src/main/java/com/wqj/cloudserver/rpc/impl/RpcUserServiceImpl.java
+++ cloud-provider/src/main/java/com/wqj/cloudserver/rpc/impl/RpcUserServiceImpl.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.RestController;
  * @Description:
  */
 @RestController
-@EnableDiscoveryClient
 public class RpcUserServiceImpl {
 
     @Autowired
